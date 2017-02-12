e0b42e76318a2d8555967f97a55dabefb4212aba
mmeder2016 <mmeder2016@gmail.com>
2017-02-12 15:46:55 -0600

    Configured sessionStore in session

--- server.js
+++ server.js
@@ -71,10 +71,11 @@ var passport = require('passport');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var MySQLStore = require('express-mysql-session')(session);
+var sessionStore = new MySQLStore(options);
 
 app.use(require('connect-multiparty')());
 app.use(cookieParser());
-app.use(session({ secret: 'super-secret', resave: false, saveUninitialized: true }));
+app.use(session({ secret: 'super-secret', resave: false, saveUninitialized: true, store: sessionStore }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.set('passport', passport);
